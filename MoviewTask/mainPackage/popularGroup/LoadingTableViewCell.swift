144409ae675216b18f5c6733ae53ee76de815037
mohamedemad <apps@baianat.com>
2019-01-14 17:21:10 +0200

    feat: make infinite scroll for the popularTableView

---
+++ MoviewTask/mainPackage/popularGroup/LoadingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingTableViewCell.swift
+//  MoviewTask
+//
+//  Created by Mohamed Emad on 1/14/19.
+//  Copyright © 2019 Baianat. All rights reserved.
+//
+
+import UIKit
+
+class LoadingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
